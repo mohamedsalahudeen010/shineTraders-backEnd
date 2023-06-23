341ce9983b9e4d63de2655d9d0d8c138794b8143
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-06-23 12:11:20 +0530

    Update adminSignUpRoute.js

--- Router/Admin/adminSignUpRoute.js
+++ Router/Admin/adminSignUpRoute.js
@@ -1,17 +1,18 @@
 import express from "express"
 import bcrypt from "bcrypt"
-import { User } from "../../Model/User.js"
+import { Admin } from "../../Model/admin.js"
+
 
 const router=express.Router()
 
 router.post("/",async(req,res)=>{
     try {
-       let user= await User.findOne({email:req.body.email})
-       if(user){return res.status(409).json({message:"Email Already Exist"})}
+       let admin= await Admin.findOne({email:req.body.email})
+       if(admin){return res.status(409).json({message:"Email Already Exist"})}
        
        const salt=await bcrypt.genSalt(10);
        const hashedPassword=await bcrypt.hash(req.body.password,salt);
-       user = await new User({
+       admin = await new Admin({
         firstName:req.body.firstName,
         lastName:req.body.lastName,
         email:req.body.email,
