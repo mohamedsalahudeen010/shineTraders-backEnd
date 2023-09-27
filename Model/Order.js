5e4adfdeb6ae437df875da315539a47ca0386721
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-09-27 10:23:34 +0530

    Update Order.js

--- Model/Order.js
+++ Model/Order.js
@@ -9,30 +9,11 @@ const orderSchema=mongoose.Schema({
         type:String,
         required:true
     },
-    productName:{
-        type:String,
+    products:{
+        type:[],
         required:true
     },
-    productImage:{
-        type:String,
-        required:true
-    },
-    productVarient:{
-        type:Number,
-        required:true
-    },
-    productQuantity:{
-        type:Number,
-        required:true
-    },
-    productPricePerItem:{
-        type:Number,
-        required:true
-    },
-    totalPrice:{
-        type:Number,
-        required:true
-    },
+    
 
     
 })
